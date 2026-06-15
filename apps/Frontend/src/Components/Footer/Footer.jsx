16119785a742bb7cb14d8bd92a031dd9cc69427c
yousif <youssefamr4331@gmail.com>
2026-06-16 02:38:32 +0300

    Update Footer.jsx

--- apps/Frontend/src/Components/Footer/Footer.jsx
+++ apps/Frontend/src/Components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../Image/logo.png"; // 👈 نفس مسار اللوجو عندك
 import "../Footer/Footer.css";
+
 export default function Footer() {
   return (
     <footer className="footer">
@@ -18,11 +20,18 @@ export default function Footer() {
             </p>
 
             <div className="social-icons d-flex gap-3">
-              {/* ⚠️ في React لازم className مش class */}
-              <i className="fa-brands fa-x-twitter"></i>
-              <i className="fa-brands fa-facebook"></i>
-              <i className="fa-brands fa-instagram"></i>
-              <i className="fa-brands fa-linkedin"></i>
+              <a href="https://x.com" target="_blank" rel="noopener noreferrer" style={{ color: "inherit" }}>
+                <i className="fa-brands fa-x-twitter"></i>
+              </a>
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" style={{ color: "inherit" }}>
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" style={{ color: "inherit" }}>
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" style={{ color: "inherit" }}>
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
             </div>
           </div>
 
@@ -30,10 +39,10 @@ export default function Footer() {
           <div className="col-md-3 mb-3">
             <h6 className="footer-title">Heart Disease</h6>
             <ul className="footer-list">
-              <li>Heart Care</li>
-              <li>Health Care</li>
-              <li>About Us</li>
-              <li>Contact Us</li>
+              <li><Link to="/prediction" style={{ color: "inherit", textDecoration: "none" }}>Heart Care</Link></li>
+              <li><Link to="/ecg" style={{ color: "inherit", textDecoration: "none" }}>Health Care</Link></li>
+              <li><Link to="/learnmore" style={{ color: "inherit", textDecoration: "none" }}>About Us</Link></li>
+              <li><Link to="/profile" style={{ color: "inherit", textDecoration: "none" }}>Contact Us</Link></li>
             </ul>
           </div>
 
@@ -41,10 +50,10 @@ export default function Footer() {
           <div className="col-md-3 mb-3">
             <h6 className="footer-title">Labs</h6>
             <ul className="footer-list">
-              <li>AlMokhtabar</li>
-              <li>Al Borg</li>
-              <li>Hassab</li>
-              <li>Al Shams</li>
+              <li><a href="https://almokhtabar.com/ar/%d8%a7%d9%84%d9%81%d8%b1%d9%88%d8%b9/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>AlMokhtabar</a></li>
+              <li><a href="https://alborglab.com/branches/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>Al Borg</a></li>
+              <li><a href="https://hassab.com/site/ar/%D9%81%D8%B1%D9%88%D8%B9%D9%86%D8%A7/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>Hassab</a></li>
+              <li><a href="http://alshamslabs.com/branches.aspx" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>Al Shams</a></li>
             </ul>
           </div>
 
@@ -52,10 +61,10 @@ export default function Footer() {
           <div className="col-md-3 mb-3">
             <h6 className="footer-title">Resources</h6>
             <ul className="footer-list">
-              <li>AHA</li>
-              <li>CDC</li>
-              <li>NHLBI</li>
-              <li>HFSA</li>
+              <li><a href="https://www.heart.org/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>AHA</a></li>
+              <li><a href="https://www.cdc.gov/heartdisease/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>CDC</a></li>
+              <li><a href="https://www.nhlbi.nih.gov/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>NHLBI</a></li>
+              <li><a href="https://hfsa.org/" target="_blank" rel="noopener noreferrer" style={{ color: "inherit", textDecoration: "none" }}>HFSA</a></li>
             </ul>
           </div>
         </div>
