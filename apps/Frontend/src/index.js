0dc2984e791d0e81296a7e8fac3b58d261fcc776
yousif <youssefamr4331@gmail.com>
2026-06-15 08:23:12 +0300

    Update index.js

--- apps/Frontend/src/index.js
+++ apps/Frontend/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
