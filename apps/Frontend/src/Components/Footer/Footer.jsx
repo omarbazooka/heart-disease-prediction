9d79c941842984c06713c357f923d4caebae0ed9
yousif <youssefamr4331@gmail.com>
2026-06-16 17:49:53 +0300

    Update Footer.jsx

--- apps/Frontend/src/Components/Footer/Footer.jsx
+++ apps/Frontend/src/Components/Footer/Footer.jsx
@@ -46,7 +46,7 @@ export default function Footer({ isDashboard }) {
             <ul className="footer-list">
               <li><Link to="/prediction" style={{ color: "inherit", textDecoration: "none" }}>Heart Care</Link></li>
               <li><Link to="/ecg" style={{ color: "inherit", textDecoration: "none" }}>Health Care</Link></li>
-              <li><Link to="/learnmore" style={{ color: "inherit", textDecoration: "none" }}>About Us</Link></li>
+              <li><Link to="/about" style={{ color: "inherit", textDecoration: "none" }}>About Us</Link></li>
               <li><Link to="/profile" style={{ color: "inherit", textDecoration: "none" }}>Contact Us</Link></li>
             </ul>
           </div>
